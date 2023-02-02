d25692373eee54d8a4d2565f74bc27f96e9b52c2
codekyng <teryima127@gmail.com>
2023-02-02 18:35:44 +0100

    block by number

--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 
 import (
-	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
+	"fmt"
+	"context"
 )
 
-var infuralUrl = "https://mainnet.infura.io/v3/407f60619ec14e538991ba8f9e0f4237"
+var (
+    ctx         = context.Background()
+    url         = "https://mainnet.infura.io/v3/407f60619ec14e538991ba8f9e0f4237"
+    client, err = ethclient.DialContext(ctx, url)
+)
 
 func main()  {
 	// create eth client
-	client, err := ethclient.DialContext(context.Background(), infuralUrl)
+	client, err := ethclient.DialContext(ctx, url)
 
 	if err != nil {
 		log.Fatalf("Error to create an ether clent")
 	}
 	defer client.Close()
+
+	// Get block by number
+	block, err := client.BlockByNumber(ctx, nil)
+	if err != nil {
+		log.Fatalf("Error to get block:%v", err)
+	}
+	fmt.Println(block.Number())
 
 	
 	
