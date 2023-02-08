a704a290bad000cfdd5fedfafe0431b28b8bf3a1
codekyng <teryima127@gmail.com>
2023-02-08 21:00:54 +0100

    decrypting keystore

--- go-ethereum-tutorial/keystore/keystore.go
+++ go-ethereum-tutorial/keystore/keystore.go
@@ -6,6 +6,8 @@ import (
 	"log"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 // Generate a keystore wallet - Enccypted private key
@@ -25,9 +27,17 @@ func main() {
 
 	// Decrypt the file and get the private key
 
-	b, err := ioutil.ReadFile("/wallet/UTC--2023-02-06T19-31-00.372104270Z--ad272b84ce085aba470b1b1f46c8f30e31ec80d5")
+	file, err := ioutil.ReadFile("/wallet/UTC--2023-02-06T19-31-00.372104270Z--ad272b84ce085aba470b1b1f46c8f30e31ec80d5")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	key, err := keystore.DecryptKey(file, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pData := crypto.FromECDSA(key.PrivateKey)
+	hexutil.Encode(pData)
 
 }
