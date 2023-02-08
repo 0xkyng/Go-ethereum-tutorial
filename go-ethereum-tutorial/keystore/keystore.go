ef52ae869e1fb911d0469c49dcc03e9d2adb5495
codekyng <teryima127@gmail.com>
2023-02-08 20:49:26 +0100

    decrypting keystore

--- go-ethereum-tutorial/keystore/keystore.go
+++ go-ethereum-tutorial/keystore/keystore.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
@@ -14,11 +15,19 @@ import (
 // He knows the password used to encrypt it
 
 func main() {
-	key := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
+	// key := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
 	password := "password"
-	account, err := key.NewAccount(password)
+	// account, err := key.NewAccount(password)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(account.Address)
+
+	// Decrypt the file and get the private key
+
+	b, err := ioutil.ReadFile("/wallet/UTC--2023-02-06T19-31-00.372104270Z--ad272b84ce085aba470b1b1f46c8f30e31ec80d5")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(account.Address)
+
 }
