7fc0f15e617665dcdc1161e910723c15b0533249
codekyng <teryima127@gmail.com>
2023-02-09 22:07:18 +0100

    address check

--- go-ethereum-tutorial/address/address.go
+++ go-ethereum-tutorial/address/address.go
@@ -9,3 +9,13 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
+
+re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+
+fmt.Printf("is valid: %v\n", re.MatchString("0x323b5d4c32345ced77393b3530b1eed0f346429d")) // is valid: true
+fmt.Printf("is valid: %v\n", re.MatchString("0xZYXb5d4c32345ced77393b3530b1eed0f346429d")) // is valid: false
+
+client, err := ethclient.Dial("https://mainnet.infura.io/v3/407f60619ec14e538991ba8f9e0f4237")
+if err != nil {
+	log.Fatal(err)
+}
