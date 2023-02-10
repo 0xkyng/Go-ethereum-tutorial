61554abd36c78817ccd3038f83471b3f884f3d5c
codekyng <teryima127@gmail.com>
2023-02-10 16:36:16 +0100

    encrypted wallet

--- go-ethereum-tutorial/get-free-ether/get-ether.go
+++ go-ethereum-tutorial/get-free-ether/get-ether.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+)
+
 func main() {
 	// Create an encrypted wallet
 	// Using keystore
+	ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.LightScryptP)
+	_, err := ks.NewAccount("secret")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = ks.NewAccount("secret")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// "00397e3b5f78291709b945d792ad959f49667c0c"
+	// "f3e80494e6e77f2d19e7f76abac8912abf153a5d"
 
 }
