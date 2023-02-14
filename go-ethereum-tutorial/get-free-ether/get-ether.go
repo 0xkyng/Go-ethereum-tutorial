716541aa9c4d158b0dd55ebb5dc964766a8b0f29
codekyng <teryima127@gmail.com>
2023-02-14 15:34:30 +0100

    eth transfer

--- go-ethereum-tutorial/get-free-ether/get-ether.go
+++ go-ethereum-tutorial/get-free-ether/get-ether.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var (
+	url = "https://goerli.infura.io/v3/f7cdc50f468747ec96cdd15ca75141ae"
 )
 
 func main() {
 	// Create an encrypted wallet
 	// Using keystore
-	ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.LightScryptP)
-	_, err := ks.NewAccount("secret")
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = ks.NewAccount("secret")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.LightScryptP)
+	// _, err := ks.NewAccount("secret")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// _, err = ks.NewAccount("secret")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// "00397e3b5f78291709b945d792ad959f49667c0c"
 	// "f3e80494e6e77f2d19e7f76abac8912abf153a5d"
+
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	account1 := common.HexToAddress("f3e80494e6e77f2d19e7f76abac8912abf153a5d")
+	account2 := common.HexToAddress("00397e3b5f78291709b945d792ad959f49667c0c")
+
+	account1Balance := client.BalanceAt(context.Background(), account1, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
